8217e0c32ed681fec1474b5ad7dcbd19b1d3b8e4
dreamscached <dreamscache.d@gmail.com>
2021-11-29 20:28:58 +0100

    Restructure

---
+++ 26-11-2021/classwork/array of 3 items/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main(void) {
+    int p1[] = {5000, 2000, 200};
+    int *p2 = calloc(sizeof(int), 12);
+
+    int c;
+    if (scanf("%d", &c) != 1) {
+        printf("Chyba!\n");
+    }
+
+    p2[0] = p1[0];
+    int j = 1;
+    for (int i = 0; i < 3; i++) {
+        int x = p1[i];
+        while (x > 0) {
+            if (x / 1000 == 5) {
+                x = 2000;
+            } else if (x / 100 == 5) {
+                x = 200;
+            } else if (x / 10 == 5) {
+                x = 20;
+            } else {
+                x /= 2;
+            }
+
+            p2[j++] = x;
+        }
+    }
+
+    for (int i = 0; i < 11; i++) {
+        if (c >= p2[i]) {
+            printf("%4d Kč ........ %d\n", p2[i], c / p2[i]);
+            c %= p2[i];
+        }
+    }
+    printf("%4d Kč ........ %d\n", 1, c);
+}
